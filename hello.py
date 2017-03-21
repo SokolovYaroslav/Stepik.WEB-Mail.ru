7a76f53fe7b14a66ed19624edbf16fc27ec28ae0
Yaroslav Sokolov <sokolovyaroslavmm@gmail.com>
2017-03-21 22:40:11 +0300

    1.9 attempt 3

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def wsgi_application(environ, start_responce):
-	body = environ['QUERY_STRING'].replace('&', '\n')
+	body = [bytes(i+'\n','ascii') for i in data.split('&')]
 	status = '200 OK'
 	headers = [
 		('Content-Type', 'text/plain')
