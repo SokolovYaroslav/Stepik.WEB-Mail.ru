81fb763f55830dbf9aca6c897c071dec6bb18d5e
Yaroslav Sokolov <sokolovyaroslavmm@gmail.com>
2017-03-24 11:44:09 +0300

    1.9 test

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo ln -sf /home/box/web/etc/guniconf.py   /etc/gunicorn.d/guniconf
+sudo ln -sf /home/box/web/etc/guniconf-hello.conf   /etc/gunicorn.d/guniconf_hello
 sudo /etc/init.d/gunicorn restart
-sudo gunicorn -c /etc/gunicorn.d/guniconf hello:wsgi_application
+sudo gunicorn -c /etc/gunicorn.d/guniconf_hello
