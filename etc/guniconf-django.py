71e8b98d6967a992368be7b20c917da4ab22ddd1
Yaroslav Sokolov <sokolovyaroslavmm@gmail.com>
2017-03-24 19:53:51 +0300

    2.1 attempt 1

---
+++ etc/guniconf-django.py
@@ -0,0 +1,5 @@
+bind = '0.0.0.0:8000'
+workers = 2
+errorlog = 'err.log'
+accesslog = 'acc.log'
+pythonpath = '/home/box/web/ask/ask/'
--- init.sh
+++ init.sh
@@ -1,6 +1,14 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo pip3 install --upgrade django
+sudo pip3 install --upgrade gunicorn
+sudo nano /usr/sbin/gunicorn-debian
+sudo nano /usr/bin/gunicorn
+sudo nano /usr/bin/gunicorn_django
+sudo nano /usr/bin/gunicorn_paster
 sudo ln -sf /home/box/web/etc/guniconf-hello.py   /etc/gunicorn.d/guniconf_hello
+sudo ln -sf /home/box/web/etc/guniconf-django.py   /etc/gunicorn.d/guniconf_django
 sudo /etc/init.d/gunicorn restart
 sudo gunicorn -c /etc/gunicorn.d/guniconf_hello hello:wsgi_application
+sudo gunicorn -c /etc/gunicorn.d/guniconf_django wsgi:application
