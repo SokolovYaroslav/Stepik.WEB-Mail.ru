6f8ace7192e7c8e8f642bb6e42818e7d335accea
Yaroslav Sokolov <sokolovyaroslavmm@gmail.com>
2017-03-27 14:33:13 +0300

    2.1 test3

--- init.sh
+++ init.sh
@@ -2,11 +2,6 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo pip3 install --upgrade django
-sudo pip3 install --upgrade gunicorn
-sudo nano /usr/sbin/gunicorn-debian
-sudo nano /usr/bin/gunicorn
-sudo nano /usr/bin/gunicorn_django
-sudo nano /usr/bin/gunicorn_paster
 sudo gunicorn -c /home/box/web/etc/guniconf-hello.py hello:wsgi_application
 cd ask/
 sudo gunicorn -c /home/box/web/etc/guniconf-django.py ask.wsgi:application
