e660bc44b4e257e9b682044366e5dffa9bf641f5
Yaroslav Sokolov <sokolovyaroslavmm@gmail.com>
2017-03-27 14:41:18 +0300

    2.1 fix

--- init.sh
+++ init.sh
@@ -1,7 +1,6 @@
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
-sudo pip3 install --upgrade django
 sudo gunicorn -c /home/box/web/etc/guniconf-hello.py hello:wsgi_application
 cd ask/
 sudo gunicorn -c /home/box/web/etc/guniconf-django.py ask.wsgi:application
