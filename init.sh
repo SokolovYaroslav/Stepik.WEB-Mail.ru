55c0c215e470b8c9812f3cdc7a34f0978a04e4f3
Yaroslav Sokolov <sokolovyaroslavmm@gmail.com>
2017-03-27 14:47:27 +0300

    2.1 test4

--- init.sh
+++ init.sh
@@ -2,5 +2,4 @@ sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -c /home/box/web/etc/guniconf-hello.py hello:wsgi_application
-cd ask/
 sudo gunicorn -c /home/box/web/etc/guniconf-django.py ask.wsgi:application
