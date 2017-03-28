d798a51da22181c99d9baca13c6f5e1fd2e6e34c
Yaroslav Sokolov <sokolovyaroslavmm@gmail.com>
2017-03-28 23:45:31 +0300

    2.3 attempt2

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,8 +13,8 @@ class Question(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField()
     rating = models.IntegerField(default = 0)
-    author = models.ForeignKey(User)
-    likes = models.ManyToManyField(User)
+    author = models.ForeignKey(User, related_name = 'author')
+    likes = models.ManyToManyField(User, related_name = 'like')
 
 class Answer(models.Model):
     text = models.TextField()
