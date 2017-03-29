91b7e98e0d9896d9052b1266b2fb7be44c778c28
Yaroslav Sokolov <sokolovyaroslavmm@gmail.com>
2017-03-29 20:56:06 +0300

    2.3 without mysql

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'ask.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'web_proj_db',
-        'USER': 'django',
-        'PASSWORD': 'qwerty123',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
